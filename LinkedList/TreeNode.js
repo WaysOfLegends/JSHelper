900fd44e3f6053ccee415771bdabe2b2282841ca
Abhishek Gowda <abhi.gowda28@gmail.com>
2019-12-04 12:53:25 +0530

    Updated TreeNode.js

--- LinkedList/TreeNode.js
+++ LinkedList/TreeNode.js
@@ -1,4 +1,46 @@
-const TreeNode = (value) =>{
-	this.data = value;
-	this.next = null;
+function TreeNode(value) {
+    this.data = value;
+    this.next = null;
 }
+
+function LinkedList() {
+    this._length = 0;
+    this.head = null;
+
+    this.insert = function (value) {
+        let node = new TreeNode(value);
+        let currentNode = this.head;
+
+        if (!currentNode) {
+            this.head = node;
+            this._length++;
+            return;
+        }
+
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = node;
+        this._length++;
+    }
+
+    this.traverse = function () {
+
+        let currentNode = this.head;
+
+        while (currentNode) {
+            console.log(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    }
+
+}
+
+let linked = new LinkedList();
+linked.traverse();
+
+linked.insert(1);
+linked.insert(2);
+linked.insert(3);
+
+linked.traverse();
