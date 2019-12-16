722a48a1e7fd6a9bfdbead7c1c020377b0a59731
Abhishek Gowda <abhi.gowda28@gmail.com>
2019-12-16 12:29:59 +0530

    Create Regex.js

---
+++ Regex.js
@@ -0,0 +1,9 @@
+enum RegularExpression {
+    InformationalClassResponse = "\\b10[0-2]\\b",
+    SuccessClassResponse = "\\b2(?:0[0-8]|26)\\b",
+    RedirectionClassResponse = "\\b30[0-578]\\b",
+    ClientErrorResponse = "\\b4(?:0/d|1[0-8]|2[1-4689]|31|4{2}|51|9{2})\\b",
+    ServerErrorResponse = "\\b5(?:0[08]|1[01]|9{2})\\b"
+}
+
+export RegularExpression;
